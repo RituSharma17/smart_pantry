ca4f2f3088aa46aa1f703379bc7573c69c270c41
RituSharma17 <rs10892623@sju.edu>
2026-04-27 19:01:38 -0400

    first commit

---
+++ notebooks/sql_openai_config.py
@@ -0,0 +1,23 @@
+def get_mysql_config():
+    return {
+        "host": "localhost",
+        "port": 3306,
+        "user": "pantry_user",
+        "password": "MyStrongPwd123!",
+        "database": "smart_pantry",
+    }
+
+    # return {
+    #     "host": "localhost",
+    #     "port": 3306,
+    #     "user": "pantry_user",
+    #     "password": "root",
+    #     "database": "smart_pantry",
+    # }
+
+
+def get_openai():
+return "DUMMY"
+
+
+
